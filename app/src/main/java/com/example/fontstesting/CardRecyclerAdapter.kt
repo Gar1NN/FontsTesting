c069dfc3827f788ceb3636f3143101ec27c085e3
RedkozubovDS <RedkozubovDS@mail.skblab.ru>
2021-04-26 11:27:08 +0500

    перенос стилей2

--- app/src/main/java/com/example/fontstesting/CardRecyclerAdapter.kt
+++ app/src/main/java/com/example/fontstesting/CardRecyclerAdapter.kt
@@ -15,12 +15,7 @@ class CardRecyclerAdapter(private val items: ArrayList<CardViewClass>):
 
     class CardViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         var category: TextView = itemView.findViewById(R.id.Category)
-        val typeface: TextView = itemView.findViewById(R.id.Typeface)
-        val font: TextView = itemView.findViewById(R.id.Font)
-        val case: TextView = itemView.findViewById(R.id.Case)
-        val size: TextView = itemView.findViewById(R.id.Size)
-        val lineHeight: TextView = itemView.findViewById(R.id.LineHeight)
-        val letterSpacing: TextView = itemView.findViewById(R.id.LetterSpacing)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
@@ -31,25 +26,10 @@ class CardRecyclerAdapter(private val items: ArrayList<CardViewClass>):
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val item = items[position]
-        if(item._case == "UPPER"){
-            holder.category.text = item._category.toUpperCase()
-        }
-        else{
-            holder.category.text = item._category
-        }
+        holder.category.setTextAppearance(item.style)
+        holder.category.text = item.category
 
-        holder.category.setLineSpacing((item._lineHeight - item._size).toFloat(),1F)
 
-        holder.category.letterSpacing = item._letterSpacing.toFloat()
-        holder.category.textSize = item._size.toFloat()
-        if(item._font == "Medium")
-            holder.category.typeface = Typeface.create("sans-serif-medium", Typeface.NORMAL)
-
-        holder.font.text = item._font
-        holder.typeface.text = item._typeface
-        holder.size.text = "Size " + item._size.toString()
-        holder.letterSpacing.text = "Letter spacing " + item._letterSpacing.toString()
-        holder.lineHeight.text = "Line height "  + item._lineHeight.toString()
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/fontstesting/CardViewClass.kt
+++ app/src/main/java/com/example/fontstesting/CardViewClass.kt
@@ -1,23 +1,5 @@
 package com.example.fontstesting
 
-class CardViewClass(
-    category: String,
-    typeface: String, font: String, case: String, size: Int, lineHeight: Int, letterSpacing: Double) {
-    var _category = ""
-    var _font = ""
-    var _case = ""
-    var _size = 0
-    var _lineHeight = 0
-    var _letterSpacing = 0.0
-    var _typeface = ""
-    init {
-        _category =category
-        _font = font
-        _case = case
-        _size = size
-        _lineHeight = lineHeight
-        _letterSpacing = letterSpacing
-        _typeface = typeface
-    }
-
-}
+data class CardViewClass(
+    val category: String,
+    val style: Int)
